d6a98536f4215feb1494b463f262eb76fb7e532d
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-03-15 17:20:08 +0100

    Make a Basic Application

---
+++ source/acme/Acme/App/Blog.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * 
+ * Generic application.
+ * 
+ */
+class Acme_App_Blog extends Acme_Controller_Page
+{
+    /**
+     * 
+     * The default action when no action is specified.
+     * 
+     * @var string
+     * 
+     */
+    protected $_action_default = 'index';
+    
+    /**
+     * 
+     * Generic index action.
+     * 
+     * @return void
+     * 
+     */
+    public function actionIndex()
+    {
+        
+    }
+}
