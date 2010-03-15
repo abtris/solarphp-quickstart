1e865f522cd5b6da900b7e0280449645de8123ee
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-03-15 17:21:59 +0100

    Basic Application Setup

--- source/acme/Acme/App/Blog.php
+++ source/acme/Acme/App/Blog.php
@@ -6,6 +6,22 @@
  */
 class Acme_App_Blog extends Acme_Controller_Page
 {
+
+    protected $_action_default = 'index';
+
+    protected $_model;
+
+    public $list;
+
+    public $item;
+
+    public $form;
+
+    protected function _setup()
+    {
+        parent::_setup();
+        $this->_model = Solar_Registry::get('model_catalog');
+    }
     /**
      * 
      * The default action when no action is specified.
