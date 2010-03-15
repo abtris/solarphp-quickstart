18666073fd2644022c6115f727434b44183fb551
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-03-15 17:34:55 +0100

    Set Locale Strings

--- source/acme/Acme/App/Blog/Locale/en_US.php
+++ source/acme/Acme/App/Blog/Locale/en_US.php
@@ -1,4 +1,18 @@
-<?php
+<?php 
+
 return array(
-    'TEXT_WELCOME' => 'Welcome to your new application.',
+    'ERR_NO_ID_SPECIFIED'    => 'No ID was specified',
+    'ERR_NO_RECORDS'         => 'No records found.',
+    'ERR_NO_SUCH_ITEM'       => 'That item does not exist.',
+    'HEADING_ADD'            => 'Add New Article',
+    'HEADING_DELETE'         => 'Delete This Article?',
+    'HEADING_DRAFTS'         => 'Draft Articles',
+    'HEADING_EDIT'           => 'Edit Article',
+    'HEADING_INDEX'          => 'Published Articles',
+    'TEXT_DELETED'           => 'Deleted the article.',
+    'TITLE_ADD'              => 'Blog : Add New Article',
+    'TITLE_DELETE'           => 'Blog : Delete Article',
+    'TITLE_DRAFTS'           => 'Blog : Draft Articles',
+    'TITLE_EDIT'             => 'Blog : Edit Article',
+    'TITLE_INDEX'            => 'Blog : Published Articles',
 );
