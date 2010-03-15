69fe319d4d2785b974d9065840efb646c57b9b46
Ladislav Prskavec <ladislav.prskavec@gmail.com>
2010-03-15 17:13:05 +0100

    Solar 1.0.0 unpack

---
+++ source/solar/Solar/Cli/MakeDocs/Info/options.php
@@ -0,0 +1,20 @@
+<?php
+return array(
+    'source' => array(
+        'descr'   => 'The source directory, typically the PEAR directory.',
+        'param'   => 'optional',
+    ),
+    'class_dir' => array(
+        'descr'   => 'Write class API docs to this directory.',
+        'param'   => 'required', 
+    ),
+    'package_dir' => array(
+        'descr'   => 'Write package docs to this directory.',
+        'param'   => 'required', 
+    ),
+    'lint' => array(
+        'descr'   => 'Do not make docs, just lint the sources and report errors.',
+        'value'   => false,
+        'filters' => array('validateBool', 'sanitizeBool'),
+    ),
+);
